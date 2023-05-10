fe17088c42c1b8c67b03f3ec41f7573c121096b8
Kafka Khalis <khaliskafka17@gmail.com>
2023-05-10 23:28:16 +0700

    Add Validation

--- EComm_2011501158/Shared/Produk.cs
+++ EComm_2011501158/Shared/Produk.cs
@@ -7,20 +7,30 @@ namespace EComm_2011501158.Shared
     {
         [Key]
         public int IdProduk { get; set; }
+        [Required(ErrorMessage ="Nama Harus Diisi")]
         public string Nama { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Deskripsi Harus Diisi")]
         public string Deskripsi { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Gamabar Harus Diisi")]
+
         public string GambarUrl { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Harga Harus Diisi")]
         [Column(TypeName ="decimal(18,2)")]
         public decimal Harga { get; set; }
+        [Required(ErrorMessage = "Harga Original Harus Diisi")]
         [Column(TypeName = "decimal(18,2)")]
         public decimal HargaOrginal { get; set; }
+        [Required(ErrorMessage = "status Harus Diisi")]
         public bool IsPublic { get; set; }
+        [Required(ErrorMessage = "Status Harus Diisi")]
         public bool IsDeleted { get; set; }
-        public DateTime DateCreated { get; set; }
-        public DateTime DateUpdated { get; set; }
+  
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime DateUpdated { get; set; } = DateTime.Now;
 
         [ForeignKey(nameof(IdKategori))]
-        public Kategori? Kategori { get; set; } 
+        public Kategori? Kategori { get; set; }
+        [Required(ErrorMessage = "Kategori Harus Diisi")]
         public int IdKategori { get; set; }
        
     }
